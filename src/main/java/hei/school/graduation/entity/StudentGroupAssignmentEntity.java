ace7f38e9711459e059ef7bac561a9bbb22115a5
Dev-Njaina <hei.andraina.4@gmail.com>
2026-08-13 20:30:41 +0300

    feat: add StudentGroupAssignment entity, model, mapper and repository
    * feat: add StudentGroupAssignment record model
    
    * feat: add StudentGroupAssignment enity JPA
    
    * feat: add StudentGroupAssignment mapper
    
    * feat: add StudentGroupAssignment repository

---
+++ src/main/java/hei/school/graduation/entity/StudentGroupAssignmentEntity.java
@@ -0,0 +1,38 @@
+package hei.school.graduation.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "student_group_assignment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentGroupAssignmentEntity {
+
+  @Id @UuidGenerator private UUID id;
+
+  @Column(name = "student_id", nullable = false)
+  private UUID studentId;
+
+  @Column(name = "group_id", nullable = false)
+  private UUID groupId;
+
+  @Column(name = "semestre_id", nullable = false)
+  private UUID semestreId;
+
+  @Column(name = "date_debut", nullable = false)
+  private LocalDate dateDebut;
+
+  @Column(name = "date_fin")
+  private LocalDate dateFin;
+}
