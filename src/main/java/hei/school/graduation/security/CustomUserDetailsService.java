367a72552b19d60af8a62de1144586750e0758df
Andraina <hei.andraina.4@gmail.com>
2026-08-13 22:40:02 +0300

    feat: add CustomUserDetailsService in security folder

---
+++ src/main/java/hei/school/graduation/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package hei.school.graduation.security;
+
+import hei.school.graduation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .map(UserPrincipal::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Aucun user pour l'email : " + email));
+    }
+}
