5df22d689393e4f8eb0ab92d9085291f1f1f8d19
Andraina <hei.andraina.4@gmail.com>
2026-08-13 22:29:34 +0300

    feat: add User repository

---
+++ src/main/java/hei/school/graduation/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package hei.school.graduation.config;
+
+public class SecurityConfig {
+}
---
+++ src/main/java/hei/school/graduation/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package hei.school.graduation.repository;
+
+import hei.school.graduation.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    Optional<UserEntity> findByEmail(String email);
+}
