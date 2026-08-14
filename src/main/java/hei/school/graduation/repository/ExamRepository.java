ef0936caa05521d4076e2429a7edb6e9411a582b
Andraina <hei.andraina.4@gmail.com>
2026-08-14 09:17:22 +0300

    feat: add findByCourseId methode in ExamRepository and create ReleveStatut enum

--- src/main/java/hei/school/graduation/repository/ExamRepository.java
+++ src/main/java/hei/school/graduation/repository/ExamRepository.java
@@ -1,7 +1,13 @@
 package hei.school.graduation.repository;
 
 import hei.school.graduation.entity.ExamEntity;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExamRepository extends JpaRepository<ExamEntity, UUID> {}
+import javax.swing.text.html.Option;
+
+public interface ExamRepository extends JpaRepository<ExamEntity, UUID> {
+    List<ExamEntity> findByCourseId(UUID courseId);
+}
