00c7a6dd7d256d7f4cd67d3eb5674afc39479563
Roxann <rofaniry@gmail.com>
2026-08-16 18:53:25 +0300

    feat(course): add findBySemester_Id to CourseRepository

--- src/main/java/hei/school/graduation/repository/CourseRepository.java
+++ src/main/java/hei/school/graduation/repository/CourseRepository.java
@@ -1,7 +1,11 @@
 package hei.school.graduation.repository;
 
 import hei.school.graduation.entity.CourseEntity;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {}
+public interface CourseRepository extends JpaRepository<CourseEntity, UUID> {
+    List<CourseEntity> findBySemester_Id(UUID semesterId);
+}
