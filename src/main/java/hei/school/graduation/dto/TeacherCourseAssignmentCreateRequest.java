f99b52752da5ea93f7b7c69eb6cc5ec344438a29
Roxann <rofaniry@gmail.com>
2026-08-16 18:58:15 +0300

    feat(teacher-course-assignment): add TeacherCourseAssignmentCreateRequest DTO

---
+++ src/main/java/hei/school/graduation/dto/TeacherCourseAssignmentCreateRequest.java
@@ -0,0 +1,7 @@
+package hei.school.graduation.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+
+public record TeacherCourseAssignmentCreateRequest(
+        @NotNull UUID teacherId, @NotNull UUID courseId, @NotNull Integer anneeAcademique) {}
