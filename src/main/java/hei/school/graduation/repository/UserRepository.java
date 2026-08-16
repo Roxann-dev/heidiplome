445e22edf8c05d40afdaab27aff2cfa3d9148272
Andraina <hei.andraina.4@gmail.com>
2026-08-16 14:23:55 +0300

    feat: add GET/users endpoint in UserController

--- src/main/java/hei/school/graduation/repository/UserRepository.java
+++ src/main/java/hei/school/graduation/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package hei.school.graduation.repository;
 
 import hei.school.graduation.entity.UserEntity;
-
+import hei.school.graduation.model.Enum.UserRole;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
-import hei.school.graduation.model.Enum.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
--- src/main/java/hei/school/graduation/service/UserService.java
+++ src/main/java/hei/school/graduation/service/UserService.java
@@ -1,20 +1,18 @@
 package hei.school.graduation.service;
 
 import hei.school.graduation.entity.UserEntity;
-import hei.school.graduation.exception.NotFoundException;
 import hei.school.graduation.model.Enum.UserRole;
 import hei.school.graduation.repository.UserRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
-    public List<UserEntity> findAll(UserRole role) {
-        return role == null ? userRepository.findAll() : userRepository.findByRole(role);
-    }
+  private final UserRepository userRepository;
+
+  public List<UserEntity> findAll(UserRole role) {
+    return role == null ? userRepository.findAll() : userRepository.findByRole(role);
+  }
 }
