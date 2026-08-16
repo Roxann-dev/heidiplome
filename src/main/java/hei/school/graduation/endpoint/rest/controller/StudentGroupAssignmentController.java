f890c49c6ffd5de32dea3596fea1683ad233599c
Dev-Njaina <hei.andraina.4@gmail.com>
2026-08-16 15:03:48 +0300

    feat(group-assignments): add POST/users/{userId}/group-assignments endpoint in StudentGroupAssignmentController
    * feat: create StudentGroupAssignmentCreateRequest dto and add assign method in StudentGroupAssignmentService
    
    * feat: add POST/users/{userId}/group-assignments endpoint in StudentGroupAssignmentController

--- src/main/java/hei/school/graduation/endpoint/rest/controller/StudentGroupAssignmentController.java
+++ src/main/java/hei/school/graduation/endpoint/rest/controller/StudentGroupAssignmentController.java
@@ -1,14 +1,15 @@
 package hei.school.graduation.endpoint.rest.controller;
 
+import hei.school.graduation.dto.StudentGroupAssignmentCreateRequest;
 import hei.school.graduation.model.StudentGroupAssignment;
 import hei.school.graduation.service.StudentGroupAssignmentService;
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,5 +21,12 @@ public class StudentGroupAssignmentController {
   @GetMapping("/{userId}/group-assignments")
   public List<StudentGroupAssignment> history(@PathVariable UUID userId) {
     return studentGroupAssignmentService.findHistory(userId);
+  }
+
+  @PostMapping("/{userId}/group-assignments")
+  public ResponseEntity<StudentGroupAssignment> assign(
+      @PathVariable UUID userId, @Valid @RequestBody StudentGroupAssignmentCreateRequest request) {
+    var result = studentGroupAssignmentService.assign(userId, request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(result);
   }
 }
