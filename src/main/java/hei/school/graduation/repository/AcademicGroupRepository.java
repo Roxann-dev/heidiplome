759af44c2a551c8ebe8f08afb96522c815f10405
Roxann <rofaniry@gmail.com>
2026-08-16 18:43:00 +0300

    feat(academic-group): add findBySemester_Id to AcademicGroupRepository

--- src/main/java/hei/school/graduation/repository/AcademicGroupRepository.java
+++ src/main/java/hei/school/graduation/repository/AcademicGroupRepository.java
@@ -1,7 +1,11 @@
 package hei.school.graduation.repository;
 
 import hei.school.graduation.entity.AcademicGroupEntity;
+
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AcademicGroupRepository extends JpaRepository<AcademicGroupEntity, UUID> {}
+public interface AcademicGroupRepository extends JpaRepository<AcademicGroupEntity, UUID> {
+    List<AcademicGroupEntity> findBySemester_Id(UUID semesterId);
+}
