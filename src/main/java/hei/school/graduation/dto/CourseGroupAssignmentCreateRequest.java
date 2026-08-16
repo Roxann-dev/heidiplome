ef3377bf0f56889b2a2810d1bec1f87e8a34a55e
Roxann <rofaniry@gmail.com>
2026-08-16 18:51:54 +0300

    feat(course): add CourseCreateRequest and CourseGroupAssignmentCreateRequest DTOs

---
+++ src/main/java/hei/school/graduation/dto/CourseGroupAssignmentCreateRequest.java
@@ -0,0 +1,7 @@
+package hei.school.graduation.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+
+public record CourseGroupAssignmentCreateRequest(
+        @NotNull UUID groupId, @NotNull UUID semestreId) {}
