52061c625d9988de162ad9cc747de78c560bb8e5
Andraina <hei.andraina.4@gmail.com>
2026-08-16 14:06:35 +0300

    feat: add LoginRequest and LoginResponse dto

---
+++ src/main/java/hei/school/graduation/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package hei.school.graduation.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank @Email String email, @NotBlank String password) {}
---
+++ src/main/java/hei/school/graduation/dto/LoginResponse.java
@@ -0,0 +1,8 @@
+package hei.school.graduation.dto;
+
+public record LoginResponse(String accessToken, String tokenType) {
+
+    public LoginResponse(String accessToken) {
+        this(accessToken, "Bearer");
+    }
+}
