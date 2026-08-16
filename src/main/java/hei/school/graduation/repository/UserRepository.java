2701977f83cbbf8e0655d0cd4790b470da8e919d
Andraina <hei.andraina.4@gmail.com>
2026-08-16 14:31:50 +0300

    feat: add create methode in UserService and create UserCreateRequest dto

--- src/main/java/hei/school/graduation/repository/UserRepository.java
+++ src/main/java/hei/school/graduation/repository/UserRepository.java
@@ -11,4 +11,8 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
   Optional<UserEntity> findByEmail(String email);
 
   List<UserEntity> findByRole(UserRole role);
+
+  boolean existsByReference(String reference);
+
+  boolean existsByEmail(String email);
 }
--- src/main/java/hei/school/graduation/service/UserService.java
+++ src/main/java/hei/school/graduation/service/UserService.java
@@ -1,8 +1,11 @@
 package hei.school.graduation.service;
 
+import hei.school.graduation.dto.UserCreateRequest;
 import hei.school.graduation.entity.UserEntity;
+import hei.school.graduation.exception.ConflictException;
 import hei.school.graduation.model.Enum.UserRole;
 import hei.school.graduation.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,9 +13,32 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
   public List<UserEntity> findAll(UserRole role) {
     return role == null ? userRepository.findAll() : userRepository.findByRole(role);
+  }
+
+  public UserEntity create(UserCreateRequest request) {
+    if (userRepository.existsByEmail(request.email())) {
+      throw new ConflictException("Email déjà utilisé : " + request.email());
+    }
+    if (userRepository.existsByReference(request.reference())) {
+      throw new ConflictException("Référence déjà utilisée : " + request.reference());
+    }
+
+    UserEntity user =
+            UserEntity.builder()
+                    .reference(request.reference())
+                    .lastName(request.lastName())
+                    .firstName(request.firstName())
+                    .email(request.email())
+                    .passwordHash(passwordEncoder.encode(request.password()))
+                    .role(request.role())
+                    .build();
+
+    return userRepository.save(user);
   }
 }
