08dcd863a3573b97d6c8207a201ee5b1a82a08f3
Dev-Njaina <hei.andraina.4@gmail.com>
2026-08-20 10:58:04 +0300

    feat(thymeleaf): Add Thymeleaf diploma view
    * feat: add css style in promotions.html
    
    * feat: add login.html and WebLoginController
    
    * feat: Add Thymeleaf diploma view and handle missing group in AnnualAverageCalculator

--- src/main/java/hei/school/graduation/endpoint/web/controller/PromotionThymeleafController.java
+++ src/main/java/hei/school/graduation/endpoint/web/controller/PromotionThymeleafController.java
@@ -1,20 +1,36 @@
 package hei.school.graduation.endpoint.web.controller;
 
+import hei.school.graduation.model.Enum.Parcours;
 import hei.school.graduation.repository.PromotionRepository;
+import hei.school.graduation.service.DiplomeListService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class PromotionThymeleafController {
 
   private final PromotionRepository promotionRepository;
+  private final DiplomeListService diplomeListService;
 
   @GetMapping("/promotions-view")
   public String listPromotions(Model model) {
     model.addAttribute("promotions", promotionRepository.findAll());
     return "promotions";
+  }
+
+  @GetMapping("/promotions/{promotionId}/diplomes/view")
+  public String viewDiplomes(
+      @PathVariable UUID promotionId, @RequestParam Parcours parcours, Model model) {
+    var promotion = promotionRepository.findById(promotionId).orElseThrow();
+    model.addAttribute("diplomes", diplomeListService.computeDiplomes(promotionId, parcours));
+    model.addAttribute("parcours", parcours.name());
+    model.addAttribute("promotionLabel", promotion.getLabel());
+    return "diplomes";
   }
 }
